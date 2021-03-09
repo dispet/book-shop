6523f4d25cfd927a8066e1006a9c61fcd0c27266
dispet <62215289+dispet@users.noreply.github.com>
2021-03-09 12:33:24 +0300

    Update order-by.pipe.ts

--- src/app/shared/pipes/order-by.pipe.ts
+++ src/app/shared/pipes/order-by.pipe.ts
@@ -4,18 +4,9 @@ import { Pipe, PipeTransform } from '@angular/core';
     name: 'orderBy',
 })
 export class OrderByPipe implements PipeTransform {
-    transform(books: any, sortParam: string, sortFlag: string): unknown {
-        if (!sortParam) {
-            return books;
-        }
-        if (!sortFlag) {
-            sortFlag = 'asc';
-        }
-        if (sortFlag === 'asc') {
-            books.sort((a, b) => a[sortParam] > b[sortParam]);
-            return books;
-        } else if (sortFlag === 'desc') {
-            return books.sort((a, b) => b[sortParam] > a[sortParam]);
-        }
+    transform(books: any, sortParam = 'name', sortFlag = 'desc'): unknown {
+        return sortFlag === 'asc'
+            ? books.sort((a, b) => a[sortParam] > b[sortParam])
+            : books.sort((a, b) => b[sortParam] > a[sortParam]);
     }
 }
